b4c4f5643c8ed4d4d1053daa7a991fc793c48b5f
KatalinaPerez <kata.perezg@duocuc.cl>
2024-08-23 13:00:29 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myAppProyect',
+  webDir: 'www'
+};
+
+export default config;
