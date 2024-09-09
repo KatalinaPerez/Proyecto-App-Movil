4260a0e411f7c0527a3dec5848449e69cd446920
Christian Cuadra <c.cuadra@duocuc.cl>
2024-09-09 20:22:31 -0300

    se agregan cards

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-
-  constructor() {}
-
+  cards = [
+    { title: 'Card 1', image: 'assets/img/card1.jpg' },
+    { title: 'Card 2', image: 'assets/img/card2.jpg' },
+    { title: 'Card 3', image: 'assets/img/card3.jpg' },
+    { title: 'Card 4', image: 'assets/img/card4.jpg' },
+    { title: 'Card 5', image: 'assets/img/card5.jpg' },
+  ];
 }
