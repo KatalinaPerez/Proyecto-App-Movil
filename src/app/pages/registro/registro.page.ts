bb06b4683b9dca56265b4b094351068498073308
KatalinaPerez <kata.perezg@duocuc.cl>
2024-09-23 12:43:40 -0300

    Actualización Registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { addIcons } from 'ionicons';
-import { eye, lockClosed } from 'ionicons/icons';
+import { eye, lockClosed} from 'ionicons/icons';
+import { Router} from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -9,16 +11,36 @@ import { eye, lockClosed } from 'ionicons/icons';
 })
 export class RegistroPage implements OnInit {
 
+  registro:any={
+    NombreUsuario:"",
+    correo:"",
+    contrasena:"",
+    confcontrasena:"",
+  }
+  field:string="";
+
   contrasena: string = 'password';
   ojo: string = 'eye-off';
 
   contrasenaConf: string = 'password';
   ojoConf: string = 'eye-off';
-  constructor() { 
-    addIcons({ eye, lockClosed });
+
+  //contrasenaError: boolean = false;
+
+  constructor(public router: Router, public toastController: ToastController) { 
+    addIcons({ eye, lockClosed});
   }
 
   ngOnInit() {
+  }
+
+  inhome(){
+    if(this.validateModel(this.registro)){
+      this.router.navigate(['/home']);
+      this.presentToast("top","Bienvenido",2000)
+    }else{
+      this.presentToast("middle","Error - Falta: "+this.field);//Mensaje de error
+    }
   }
 
   visibilidadContrasena(){
@@ -39,5 +61,30 @@ export class RegistroPage implements OnInit {
       this.contrasenaConf = 'password';
       this.ojoConf = 'eye-off';
     }
+  }
+
+  /*validarContrasenas() {
+    this.contrasenaError = this.registro.contrasena !== this.registro.confcontrasena;
+  }*/
+
+  validateModel(model:any){
+    for(var [key ,value] of Object.entries(model)){
+      if(value == ""){
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom', msg:string, duration?:number) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: duration?duration:2500,
+      position: position,
+      
+    });
+
+    await toast.present();
   }
 }
