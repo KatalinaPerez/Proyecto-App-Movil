da8fddb4ddd3f82c7ee57c72569cd8298c3feb4e
Christian Cuadra <c.cuadra@duocuc.cl>
2024-10-09 18:52:12 -0300

    arreglos menores

--- src/app/pages/autenticacion/home/home.page.ts
+++ src/app/pages/autenticacion/home/home.page.ts
@@ -1,9 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-<<<<<<< HEAD:src/app/pages/home/home.page.ts
-import { SpotifyService } from '../../service/api-spotify.service';
-=======
 import { SpotifyService } from '../../../service/api-spotify.service'
->>>>>>> master:src/app/pages/autenticacion/home/home.page.ts
 
 @Component({
   selector: 'app-home',
@@ -21,21 +17,11 @@ export class HomePage {
     if (!trackName) {
       return;
     }
-<<<<<<< HEAD:src/app/pages/home/home.page.ts
-
-    this.hasSearched = true;
-    this.spotifyService.searchTrack(trackName).subscribe(
-      (response) => {
-        this.tracks = response.tracks.items;
-        console.log(this.tracks);
-=======
-  
     this.hasSearched = true;
     this.spotifyService.searchTrack(trackName).subscribe(
       (response) => {
         console.log('API Response:', response); // Verifica lo que la API está retornando
         this.tracks = response.tracks?.items || []; // Asegúrate de que 'items' existe
->>>>>>> master:src/app/pages/autenticacion/home/home.page.ts
       },
       (error) => {
         console.error('Error:', error);
@@ -44,13 +30,9 @@ export class HomePage {
     );
   }
 
-<<<<<<< HEAD:src/app/pages/home/home.page.ts
-=======
   getArtists(artists: any[]): string {
     return artists.map(artist => artist.profile.name).join(', ');
   }
-  
->>>>>>> master:src/app/pages/autenticacion/home/home.page.ts
   cards = [
     { type: 'album', title: 'Chemtrails Over The Country Club', image: 'assets/imagenes/COCC-Lana-Del-Rey.jpg', artist: 'Lana del Rey' },
     { type: 'album', title: 'Born To Die', image: 'assets/imagenes/Lana-Del-Rey-Born-To-Die-album.jpg', artist: 'Lana del Rey' },
