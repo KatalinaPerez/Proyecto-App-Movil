8bf65413c2d44bcaa31dc1ce3c8a24cca030dcdf
KatalinaPerez <kata.perezg@duocuc.cl>
2024-12-04 16:24:04 -0300

    .

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { User } from '../models/user.model';
 
 import { AngularFirestore } from '@angular/fire/compat/firestore';
@@ -88,11 +88,6 @@ export class FirebaseService {
       throw error; // Lanzar el error para manejarlo en los métodos que llamen esta función
     }
   }
-
-
-  // Enviar emal para cambio de contraseña
-  recoverPassEmail(email:string) {
-    return sendPasswordResetEmail(getAuth(),email);
 
   // ::::::::::::::::::::: CANCIONES :::::::::::::::::::::
 
